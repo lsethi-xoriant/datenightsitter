78ad5bd5e1c6c3d1f71293b58cacb1e1af576613
Andrew Conrad <aconrad@sittercity.com>
2014-06-18 21:28:27 -0500

    working file

--- z_db/initial_rails_app.sh
+++ z_db/initial_rails_app.sh
@@ -1,7 +1,5 @@
 rails generate model Member first_name:string last_name:string address:string city:string state:string zip:string dob:date email:string phone:string
 
-rails generate model MerchantAccount processor_customer_id:string status:string
+rails generate model Transaction type:string status:string amount:int merchant_account:references payment_account:references processor_transaction_id
 
-rails generate model PaymentAccount processor_customer_id:string processor_payment_token:string status:string
-
-rails generate model Transactions type:string status:string amount:int merchant_account:references payment_account:references processor_transaction_id
+rails generate model Message type:string provider:references seeker:references sender:string subject:string body:text
